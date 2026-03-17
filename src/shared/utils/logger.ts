5580d0c90d565c08f91e990ca3590e3b279107dd
IgorVVieira <igor.gutoo63@gmail.com>
2026-03-17 16:02:29 -0300

    feat: add grafana and loki

--- src/shared/utils/logger.ts
+++ src/shared/utils/logger.ts
@@ -1,5 +1,29 @@
 import pino from 'pino';
 
-export const logger = pino({
-  level: process.env.LOG_LEVEL || 'info',
-});
+const transport =
+  process.env.NODE_ENV === 'production' && process.env.GRAFANA_LOKI_URL
+    ? pino.transport({
+        targets: [
+          { target: 'pino/file', level: 'info', options: { destination: 1 } },
+          {
+            target: 'pino-loki',
+            level: 'info',
+            options: {
+              host: process.env.GRAFANA_LOKI_URL,
+              basicAuth: {
+                username: process.env.GRAFANA_LOKI_USER || '',
+                password: process.env.GRAFANA_LOKI_PASSWORD || '',
+              },
+              labels: { app: 'zencash-api', env: process.env.NODE_ENV },
+              propsToLabels: ['level'],
+              silenceErrors: true,
+              replaceTimestamp: true,
+              timeout: 3000,
+              batching: { interval: 5 },
+            },
+          },
+        ],
+      })
+    : undefined;
+
+export const logger = pino({ level: process.env.LOG_LEVEL || 'info' }, transport);
